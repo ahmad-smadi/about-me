77d8b5aaa3a55b4264fdf8eb4467237ea181a6bc
ahmad-smadi <ahmad.smadi4@gmail.com>
2021-06-07 16:11:42 +0300

    i creat a simple webpage using html , css and js

---
+++ js/app.js
@@ -0,0 +1,49 @@
+'use strict'
+
+let userName = prompt('waht is your name');
+ console.log(userName);
+ alert(`hello ${userName}, to my own webpage `);
+
+
+ let like = prompt('do you think i like coding').toLowerCase;
+ if(like === 'yes' || 'y'){
+     console.log(like);
+     alert('awsome');
+ } else if (like === 'no' || 'n'){
+     alert('bad');
+ } else ('please choose your answer between yes, y , no and n');
+
+ let cat = prompt('do you think i like cat ?').toLowerCase;
+ if(cat === 'yes' || 'y'){
+     
+     alert('Great !');
+ } else if (cat === 'no' || 'n'){
+    console.log(cat);
+     alert('unluky you are');
+ } else ('please choose your answer between yes, y , no and n');
+
+ 
+ let car = prompt('do you think i like car ?').toLowerCase;
+ if(cat === 'yes' || 'y'){
+     console.log(car);
+     alert('amazing ');
+ } else if (car === 'no' || 'n'){
+     alert('unluky you are');
+ } else ('please choose your answer between yes, y , no and n');
+
+ let coffe = prompt('do you think i like coffe ?').toLowerCase;
+ if(cat === 'yes' || 'y'){
+     console.log(coffe);
+     alert('Great ');
+ } else if (coffe === 'no' || 'n'){
+     alert('sure no');
+ } else ('please choose your answer between yes, y , no and n');
+
+
+ let mansaf = prompt('do you think i like mansaf ?').toLowerCase;
+ if(cat === 'yes' || 'y'){
+     console.log(mansaf);
+     alert('good job');
+ } else if (mansaf === 'no' || 'n'){
+     alert('think again');
+ } else ('please choose your answer between yes, y , no and n');
