54211e55aa86685ddcd1afd7f33afd27a15fbc44
ahmad-smadi <ahmad.smadi4@gmail.com>
2021-06-11 03:46:38 +0300

    made style on the page

--- js/app.js
+++ js/app.js
@@ -5,85 +5,85 @@
  alert(`hello ${userName}, to my own webpage `);
 
 
-// let like = prompt('do you think i like coding ?').toLowerCase();
-// if (like === 'yes' || like === 'y') {
-//     console.log('correct');
-//     alert('you are correct')
-// }
-// else if (like === 'no' || like === 'n') {
+ let like = prompt('do you think i like coding ?').toLowerCase();
+ if (like === 'yes' || like === 'y') {
+     console.log('correct');
+     alert('you are correct')
+ }
+ else if (like === 'no' || like === 'n') {
        
-//     alert('uncorrect')
-// } else {
-//     alert('please chose between yes or y or no or n');
-// }
-// correctAnswer++;
+     alert('uncorrect')
+ } else {
+     alert('please chose between yes or y or no or n');
+ }
+ correctAnswer++;
 
-// let cat = prompt('do you think i like cat ?').toLowerCase();
-// if (cat === 'yes' || cat === 'y') {
+ let cat = prompt('do you think i like cat ?').toLowerCase();
+ if (cat === 'yes' || cat === 'y') {
     
-//     alert('uncorrect')
-// } else if (cat === 'no' || cat === 'n'){
-//     console.log('you are not correct');
-// alert('correct')
-// } else {
-//     alert('please chose between yes or y or no or n')
-// }
-// correctAnswer++;
+     alert('uncorrect')
+ } else if (cat === 'no' || cat === 'n'){
+     console.log('you are not correct');
+ alert('correct')
+ } else {
+     alert('please chose between yes or y or no or n')
+ }
+ correctAnswer++;
 
 
-// let car = prompt('do you think i like car ?').toLowerCase();
-// if (car === 'yes' || car === 'y') {
-//     console.log(' correct')
-//     alert(' correct')
-// } else if (car === 'no' || car === 'n') {
-//     alert('uncorrect')
-// } else {
-//     alert('please chose between yes or y or no or n')
-// }
-// correctAnswer++;
+ let car = prompt('do you think i like car ?').toLowerCase();
+ if (car === 'yes' || car === 'y') {
+     console.log(' correct')
+     alert(' correct')
+ } else if (car === 'no' || car === 'n') {
+     alert('uncorrect')
+ } else {
+     alert('please chose between yes or y or no or n')
+ }
+ correctAnswer++;
 
-// let coffe = prompt('do you think i like coffe ?').toLowerCase();
-// if (coffe === 'yes' || coffe === 'y') {
-//     console.log('correct');
-//     alert('correct')
-// } else if (coffe === 'no' || coffe === 'n') {
-//     alert('uncorrect');
-// } else {
-//     alert('please chose between yes or y or no or n')
-// }
-// correctAnswer++;
+ let coffe = prompt('do you think i like coffe ?').toLowerCase();
+ if (coffe === 'yes' || coffe === 'y') {
+     console.log('correct');
+     alert('correct')
+ } else if (coffe === 'no' || coffe === 'n') {
+     alert('uncorrect');
+ } else {
+     alert('please chose between yes or y or no or n')
+ }
+ correctAnswer++;
 
 
-// let mansaf = prompt('do you think i like mansaf ?').toLowerCase();
-// if (mansaf === 'yes' || mansaf === 'y') {
-//     console.log('correct')
-//     alert('correct')
-// } else if (mansaf === 'no' || mansaf === 'n') {
-//     alert('uncorrect');
-// } else {
-//     alert('please chose between yes or y or no or n');
-// }
-// correctAnswer++;
-//  let lucky=0 ;
-//  for(let i=0;i<4;i++){
-//     lucky=prompt('guess my lucky number');
-//     lucky=parseInt(lucky);
-//  if (lucky ===3) {
-//     console.log('correct');
-//     alert('correct');
-//     break;
-//  } else if ( lucky>3 ) {
-//      alert('too high');
-// } else 
-//      alert('too low');
-// }
+ let mansaf = prompt('do you think i like mansaf ?').toLowerCase();
+ if (mansaf === 'yes' || mansaf === 'y') {
+     console.log('correct')
+     alert('correct')
+ } else if (mansaf === 'no' || mansaf === 'n') {
+     alert('uncorrect');
+ } else {
+     alert('please chose between yes or y or no or n');
+ }
+ correctAnswer++;
+  let lucky=0 ;
+  for(let i=0;i<4;i++){
+     lucky=prompt('guess my lucky number');
+     lucky=parseInt(lucky);
+  if (lucky ===3) {
+     console.log('correct');
+     alert('correct');
+     break;
+  } else if ( lucky>3 ) {
+      alert('too high');
+ } else 
+      alert('too low');
+ }
 
-// correctAnswer++;
+ correctAnswer++;
 
   
   
-  for(let i=0; i < 6 ; i++ ) {
-    let guessNumber =  prompt('im thinking in number between 0 and 10 guess what is it')
+  for(let i=0; i <= 6 ; i++ ) {
+    let guessNumber =  prompt('im thinking in number between 0 and 10 guess what is it');
       if ( i === 3){
           alert('correct');
           break;
